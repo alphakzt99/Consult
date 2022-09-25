5d4ca7353b704b1e9e0d5051c3e6bedff4267322
Kaung Zaw Thant <kaung.thant@aiesec.net>
2022-09-25 13:12:35 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Consult());
+}
+
+class Consult extends StatefulWidget {
+  Consult({Key? key}) : super(key: key);
+
+  @override
+  State<Consult> createState() => _ConsultState();
+}
+
+class _ConsultState extends State<Consult> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
