869e8afa1eca8661a028654f92a50e8f1b0086fd
Kaung Zaw Thant <kaung.thant@aiesec.net>
2022-10-12 15:23:24 -0500

    chat complete

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -18,17 +18,70 @@ class _ChatState extends State<Chat> {
   ScrollController controller = ScrollController();
 
   List chatHeads = [
-    chatH("lib/photos/avatar.jpg"),
-    chatH("lib/photos/avatar.jpg"),
-    chatH("lib/photos/avatar.jpg"),
-    chatH("lib/photos/avatar.jpg"),
-    chatH("lib/photos/avatar.jpg"),
-    chatH("lib/photos/avatar.jpg"),
-    chatH("lib/photos/avatar.jpg"),
+    chatH("lib/photos/avatar.jpg", "Jack"),
+    chatH("lib/photos/avatar.jpg", "Kate"),
+    chatH("lib/photos/avatar.jpg", "Nabu"),
+    chatH("lib/photos/avatar.jpg", "Chris"),
+    chatH("lib/photos/avatar.jpg", "Jacob"),
+    chatH("lib/photos/avatar.jpg", "Junior"),
+    chatH("lib/photos/avatar.jpg", "Heize"),
   ];
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+
+    Widget convo() {
+      return Container(
+        width: size.width * 0.8,
+        height: size.height * 0.1,
+        child: Center(
+          child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            CircleAvatar(
+              radius: 25,
+              backgroundImage: AssetImage("lib/photos/avatar.jpg"),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Kaung Zaw Thant",
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                RichText(
+                    text: TextSpan(children: [
+                  TextSpan(
+                      text: "You sent an attachment",
+                      style: Theme.of(context).textTheme.bodyText2),
+                  TextSpan(
+                      text: "  6h",
+                      style: Theme.of(context).textTheme.bodyText2)
+                ]))
+              ],
+            )
+          ]),
+        ),
+      );
+    }
+
+    List contents = [
+      convo(),
+      convo(),
+      convo(),
+      convo(),
+      convo(),
+      convo(),
+      convo(),
+      convo(),
+      convo(),
+      convo(),
+    ];
     return Scaffold(
         body: Container(
       width: size.width,
@@ -51,22 +104,44 @@ class _ChatState extends State<Chat> {
         ),
         Container(
           width: size.width,
-          height: size.height * 0.1,
+          height: size.height * 0.15,
           child: ListView(
             padding: EdgeInsets.all(8),
             scrollDirection: Axis.horizontal,
             shrinkWrap: true,
             controller: hcontroller,
             children: [
-              IconButton(
-                  color: Theme.of(context).backgroundColor,
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all(CircleBorder())),
-                  onPressed: () {},
-                  icon: Icon(
-                    FluentIcons.search_20_filled,
-                    color: Theme.of(context).colorScheme.onTertiary,
-                  )),
+              Container(
+                width: 60,
+                height: 100,
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                Theme.of(context).primaryColorLight),
+                            shape: MaterialStateProperty.all(CircleBorder())),
+                        onPressed: () {},
+                        icon: Icon(
+                          FluentIcons.search_20_filled,
+                          color: Theme.of(context).colorScheme.onTertiary,
+                        )),
+                    Text(
+                      "Search",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: font,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
               ...chatHeads
             ],
           ),
@@ -101,85 +176,59 @@ class _ChatState extends State<Chat> {
             child: ListView.builder(
                 shrinkWrap: true,
                 controller: controller,
-                itemCount: 10,
+                itemCount: contents.length,
                 scrollDirection: Axis.vertical,
                 itemBuilder: ((context, index) {
                   return SwipeActionCell(
-                    trailingActions: [
-                      SwipeAction(
-                          backgroundRadius: 20,
-                          color: Colors.transparent,
-                          widthSpace: size.width * 0.3,
-                          content: Icon(
-                            FluentIcons.delete_20_regular,
-                            color: Theme.of(context).colorScheme.onBackground,
-                          ),
-                          onTap: (CompletionHandler handler) async {
-                            await handler(true);
-                            SnackBar(
-                              content: Column(children: [
-                                Text(
-                                  "Delete This Conversation?",
-                                  style: Theme.of(context).textTheme.headline4,
-                                ),
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text("Cancel",style: Theme.of(context).textTheme.headline4,))
-                              ]),
-                            );
-                            setState(() {});
-                          })
-                    ],
-                    backgroundColor: Colors.transparent,
-                    key: ValueKey(index),
-                    child: Container(
-                      width: size.width * 0.8,
-                      height: size.height * 0.1,
-                      child: Center(
-                        child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              CircleAvatar(
-                                radius: 25,
-                                backgroundImage:
-                                    AssetImage("lib/photos/avatar.jpg"),
+                      trailingActions: [
+                        SwipeAction(
+                            title: "Delete",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontFamily: font,
+                                fontWeight: FontWeight.bold),
+                            backgroundRadius: 20,
+                            color: Colors.transparent,
+                            widthSpace: size.width * 0.3,
+                            content: IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                FluentIcons.delete_20_regular,
                               ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "Kaung Zaw Thant",
-                                    style:
-                                        Theme.of(context).textTheme.headline3,
-                                  ),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  RichText(
-                                      text: TextSpan(children: [
-                                    TextSpan(
-                                        text: "You sent an attachment",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyText2),
-                                    TextSpan(
-                                        text: "  6h",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyText2)
-                                  ]))
-                                ],
-                              )
-                            ]),
-                      ),
-                    ),
-                  );
+                              color: Theme.of(context).colorScheme.onBackground,
+                            ),
+                            onTap: (CompletionHandler handler) async {
+                              await handler(true);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      duration: const Duration(seconds: 5),
+                                      backgroundColor:
+                                          Theme.of(context).backgroundColor,
+                                      action: SnackBarAction(
+                                        textColor: Theme.of(context)
+                                            .colorScheme
+                                            .onSecondaryContainer,
+                                        label: "Undo",
+                                        onPressed: () {},
+                                      ),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      width: size.width * 0.9,
+                                      behavior: SnackBarBehavior.floating,
+                                      content: Text(
+                                        "Deleted One Conversation",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontFamily: font,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black),
+                                      )));
+                            })
+                      ],
+                      backgroundColor: Colors.transparent,
+                      key: ValueKey(index),
+                      child: convo());
                 })),
           ),
         )
@@ -188,15 +237,32 @@ class _ChatState extends State<Chat> {
   }
 }
 
-Widget chatH(String image) {
+Widget chatH(String image, String name) {
   return Container(
-    width: 40,
-    height: 40,
+    width: 60,
+    height: 100,
     margin: EdgeInsets.all(8),
-    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-    child: CircleAvatar(
-      radius: 20,
-      backgroundImage: AssetImage(image),
+    decoration: BoxDecoration(
+        color: Colors.transparent, borderRadius: BorderRadius.circular(20)),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 20,
+          backgroundImage: AssetImage(image),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          name,
+          style: TextStyle(
+              color: Colors.white,
+              fontFamily: font,
+              fontSize: 14,
+              fontWeight: FontWeight.bold),
+        )
+      ],
     ),
   );
 }
